d1f1f122528b6a440d7340863d72f24e43974ebf
Sagar Muneshwar <129708548+Sagar1006-dev@users.noreply.github.com>
2023-12-01 17:33:52 +0530

    E-commerce Website using React and Redux

---
+++ e-commerse-website/src/pages/Product/actions/productAction.js
@@ -0,0 +1,17 @@
+
+import { callAPI} from "../../../apis/callApi"
+import { ActionTypes } from "../../../redux/constants/action-type";
+
+export const fetchProductsAction = (products) => {
+  return {
+    type: ActionTypes.FETCH_PRODUCTS,
+    payload: callAPI("GET", "/products"),
+  };
+};
+
+export const selectedProductAction = (product) => {
+  return {
+    type: ActionTypes.SELECTED_PRODUCT,
+    payload: product,
+  };
+};
